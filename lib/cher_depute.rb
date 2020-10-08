a1c5958d838a80cddf15b4c4f76acea017efb269
Antoine Stjepanovic <apbstep@gmail.com>
2020-10-08 23:27:32 +0200

    commit

--- lib/cher_depute.rb
+++
@@ -1,6 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/95/avernes.html"))
-
-puts email = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]")
