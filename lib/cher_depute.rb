48ea5bbb58bc496f813f2cbf1cb249f7ccaf01cd
Antoine Stjepanovic <apbstep@gmail.com>
2020-10-08 23:25:12 +0200

    commit

--- lib/cher_depute.rb
+++ lib/cher_depute.rb
@@ -1,2 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+
+page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/95/avernes.html"))
+
+puts email = page.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]")
--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -1,2 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
+
+
+def get_cities
+    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    return page.xpath('//tr[3]//a/text()').to_a.drop(2).to_a
+end
+
+def get_email
+    page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    email = []
+
+    page.xpath('//tr[3]//a/@href').to_a.map{|link| link.to_s.sub!(".", "")}.drop(1).to_a.each do |link|
+        page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com"+link))
+        email << page.xpath('//main//section[2]//table[1]//tbody//tr[4]//td[2]/text()')
+    end
+
+    return puts get_cities.map{|c| c.to_s}.zip(email.map{|e| e.to_s}).map{|c,e| {c => e}}.to_a
+end
+
+get_email
